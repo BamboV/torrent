57bcadea2092bb501ec2461b5aaa8b18d2f30261
Vladimir Barmotin <barmotinvladmimir@gmail.com>
2017-07-08 12:55:34 +0600

    Changed client constructor

--- clients/abstract_torrent_client/client/client.go
+++ clients/abstract_torrent_client/client/client.go
@@ -7,18 +7,20 @@ import (
 )
 
 type Client struct {
-	client http.Client
+	client    http.Client
+	clientURL string
 }
 
-func NewClient(client http.Client) Client {
+func NewClient(client http.Client, clientURL string) Client {
 	return Client{
-		client: client,
+		client:    client,
+		clientURL: clientURL,
 	}
 }
 
-func (c *Client) DownloadByMagnet(clientURL string, magnet string) bool {
+func (c *Client) DownloadByMagnet(magnet string) bool {
 	var jsonStr = []byte(fmt.Sprintf(`{"magnet":"%v"}`, magnet))
-	resp, err := c.client.Post(clientURL, "application/json", bytes.NewBuffer(jsonStr))
+	resp, err := c.client.Post(c.clientURL, "application/json", bytes.NewBuffer(jsonStr))
 
 	if err != nil {
 		return false
