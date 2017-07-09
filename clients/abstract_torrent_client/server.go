c2e6724f9b4c4c22fe7b8159b4735c8d414fce46
Vladimir Barmotin <barmotinvladmimir@gmail.com>
2017-07-09 21:04:39 +0600

    Changed request handling params

--- clients/abstract_torrent_client/server.go
+++ clients/abstract_torrent_client/server.go
@@ -8,11 +8,18 @@ import (
 type Server struct {
 }
 
+type magnet struct {
+	Magnet string `json:"magnet"`
+}
+
 func (server *Server) Start(client TorrentClient) {
 	http.HandleFunc("/magnet/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
-			magnet := r.PostForm.Get("magnet")
-			data := client.HandleMagnet(magnet)
+			decoder := json.NewDecoder(r.Body)
+			magnet := magnet{}
+
+			decoder.Decode(&magnet)
+			data := client.HandleMagnet(magnet.Magnet)
 			str, _ := json.Marshal(data)
 			w.Write(str)
 		}
